aa4a90f230db3a9b6c77dce38ed3e855579072dd
MariamMohebF <mariammohebfahmy@gmail.com>
2023-09-23 12:27:24 -0700

    1. The last digit

--- 1-last_digit.c
+++ 1-last_digit.c
@@ -8,22 +8,15 @@
  */
 int main(void)
 {
-	int n, m;
+	int n, digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	m = n % 10;
-	if (m > 5)
+	digit = n % 10;
+	if (digit > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, m);
+		printf("");
 	}
-	else if (m < 6 && m != 0)
-	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
-	}
-	else
-	{
-		printf("Last digit of %d is %d and is 0\n", n, m);
-	}
+	else if (digit == 0)
 	return (0);
 }
