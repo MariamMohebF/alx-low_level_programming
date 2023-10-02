47a6a6722012fcf4d69e7454f84cb63b598b98de
MariamMohebF <mariammohebfahmy@gmail.com>
2023-10-02 11:42:20 -0700

    4. To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -5,7 +5,6 @@
 
 /**
  * main - Program that adds positive numbers
-
  * @argc: This is the argument count
  * @argv: This is the argument vector
  *
@@ -14,14 +13,12 @@
 int main(int argc, char *argv[])
 {
 	int sum = 0, i;
-
 	if (argc > 1)
 	{
 		for (i = 1; i < argc; i++)
 		{
 			int b;
 			char *str;
-
 			str = argv[i];
 			for (b = 0; str[b] != '\0'; b++)
 			{
